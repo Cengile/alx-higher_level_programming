49f64a7463d6265e7b764354db9eac36e4769aaf
Cengile <inya.item@ncdc.gov.ng>
2023-06-01 13:34:03 +0000

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
-#!/bin/python3
+#!/usr/bin/python3
 # Author - Item Inya Item
 
 def remove_char_at(str, n):
     if n < 0:
         return (str)
-    eturn (str[:n] + str[n+1:])
+    return (str[:n] + str[n+1:])
